21eb12c937d2d7686e022c0e09e468135bddbd92
Renan de Assis Alves <renan.assis@sounuv.com>
2025-10-08 08:12:43 -0300

    Adicionada senha no redis

--- backend/core/utils/config.py
+++ backend/core/utils/config.py
@@ -280,7 +280,7 @@ class Configuration:
     # Redis configuration
     REDIS_HOST: str
     REDIS_PORT: int = 6379
-    REDIS_PASSWORD: Optional[str] = None
+    REDIS_PASSWORD: Optional[str] = 'NKQ6TRkA9ZLnxuEQMDlx82hs2R'
     REDIS_SSL: bool = True
     
     # Daytona sandbox configuration
